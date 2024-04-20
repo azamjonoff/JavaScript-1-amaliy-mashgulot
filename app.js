22cc9fb76dc357851cd9c991e8a8863503ed45e3
azamjonoff <abdullohazamjonov57gmail.com>
2024-04-20 09:38:46 +0500

    Javascript amaly mashgulot

---
+++ app.js
@@ -0,0 +1,25 @@
+// AMALIY MASHGULOT
+
+// OZIM QIGANIM
+
+// let ism = prompt("Ismingizni kiriting:");
+
+// let boshharf = ism.charAt(0).toUpperCase();
+
+// let qogani = ism.slice(1, length()).toLowerCase();
+
+// let result = boshharf + qogani;
+
+// alert(`Salom, ${boshharf}`);
+
+// LEKIN ISHLAMADI
+
+// OZIZ QIBERGANIZ
+
+let ism = prompt("Ismingizni kiriting:");
+
+let boshharf = ism.charAt(0).toUpperCase();
+let davomi = ism.slice(1).toLowerCase();
+let result = boshharf + davomi;
+
+alert(`Salom ${result}`)
